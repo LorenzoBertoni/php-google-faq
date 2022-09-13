b703df111bfd85a1b2682c47b3da409e2034b03f
LorenzoBertoni <lorenzo.berto99@hotmail.it>
2022-09-13 13:21:24 +0200

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domande frequenti - Privacy e termini - Google</title>
+    <link rel="shortcut icon" href="https://www.pngmart.com/files/16/official-Google-Logo-PNG-Image.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
